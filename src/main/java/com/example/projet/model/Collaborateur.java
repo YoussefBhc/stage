75903aab7403d9dd2705281dfc5a60310c48dc42
salimanoufel <salimanoufel22@gmail.com>
2021-09-06 15:57:08 +0100

    create classes

--- src/main/java/com/example/projet/model/Collaborateur.java
+++ src/main/java/com/example/projet/model/Collaborateur.java
@@ -25,5 +25,124 @@ public class Collaborateur implements Serializable {
     private float salaire;
 
 
+    //Constructeurs
 
+    public Collaborateur() {
+        super();
+    }
+
+    public Collaborateur(int matricule, String nomCollaborateur, String prenomCollaborateur, char sexe, String abreviation, Date dateDepart, Date dateEmbauche, Date dateParticipationSeminaireIntegration, boolean estParti, boolean participationSeminaireIntegration, float salaire) {
+        super();
+        this.matricule = matricule;
+        this.nomCollaborateur = nomCollaborateur;
+        this.prenomCollaborateur = prenomCollaborateur;
+        this.sexe = sexe;
+        this.abreviation = abreviation;
+        this.dateDepart = dateDepart;
+        this.dateEmbauche = dateEmbauche;
+        this.dateParticipationSeminaireIntegration = dateParticipationSeminaireIntegration;
+        this.estParti = estParti;
+        this.participationSeminaireIntegration = participationSeminaireIntegration;
+        this.salaire = salaire;
+    }
+
+    //les getters
+
+    public int getIdCollaborateur() {
+        return idCollaborateur;
+    }
+
+    public int getMatricule() {
+        return matricule;
+    }
+
+    public String getNomCollaborateur() {
+        return nomCollaborateur;
+    }
+
+    public String getPrenomCollaborateur() {
+        return prenomCollaborateur;
+    }
+
+    public char getSexe() {
+        return sexe;
+    }
+
+    public String getAbreviation() {
+        return abreviation;
+    }
+
+    public Date getDateDepart() {
+        return dateDepart;
+    }
+
+    public Date getDateEmbauche() {
+        return dateEmbauche;
+    }
+
+    public Date getDateParticipationSeminaireIntegration() {
+        return dateParticipationSeminaireIntegration;
+    }
+
+    public boolean isEstParti() {
+        return estParti;
+    }
+
+    public boolean isParticipationSeminaireIntegration() {
+        return participationSeminaireIntegration;
+    }
+
+    public float getSalaire() {
+        return salaire;
+    }
+
+    //les setters
+
+    public void setIdCollaborateur(int idCollaborateur) {
+        this.idCollaborateur = idCollaborateur;
+    }
+
+    public void setMatricule(int matricule) {
+        this.matricule = matricule;
+    }
+
+    public void setNomCollaborateur(String nomCollaborateur) {
+        this.nomCollaborateur = nomCollaborateur;
+    }
+
+    public void setPrenomCollaborateur(String prenomCollaborateur) {
+        this.prenomCollaborateur = prenomCollaborateur;
+    }
+
+    public void setSexe(char sexe) {
+        this.sexe = sexe;
+    }
+
+    public void setAbreviation(String abreviation) {
+        this.abreviation = abreviation;
+    }
+
+    public void setDateDepart(Date dateDepart) {
+        this.dateDepart = dateDepart;
+    }
+
+    public void setDateEmbauche(Date dateEmbauche) {
+        this.dateEmbauche = dateEmbauche;
+    }
+
+    public void setDateParticipationSeminaireIntegration(Date dateParticipationSeminaireIntegration) {
+        this.dateParticipationSeminaireIntegration = dateParticipationSeminaireIntegration;
+    }
+
+    public void setEstParti(boolean estParti) {
+        this.estParti = estParti;
+    }
+
+    public void setParticipationSeminaireIntegration(boolean participationSeminaireIntegration) {
+        this.participationSeminaireIntegration = participationSeminaireIntegration;
+    }
+
+    public void setSalaire(float salaire) {
+        this.salaire = salaire;
+    }
 }
