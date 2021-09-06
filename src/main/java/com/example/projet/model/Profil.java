100459efb21a3a34f97b685f9189229d8d9ca960
salimanoufel <salimanoufel22@gmail.com>
2021-09-06 15:57:08 +0100

    create classes

---
+++ src/main/java/com/example/projet/model/Profil.java
@@ -0,0 +1,50 @@
+package com.example.projet.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import java.io.Serializable;
+
+public class Profil implements Serializable {
+
+    @Id @GeneratedValue
+    private int idProfil;
+    private String nomProfil;
+
+    @JoinColumn(name = "idCollaborateur")
+    private Collaborateur collaborateur;
+
+    public Profil() {
+        super();
+    }
+
+    //les getters et setters
+    public Profil(String nomProfil, Collaborateur collaborateur) {
+        this.nomProfil = nomProfil;
+        this.collaborateur = collaborateur;
+    }
+
+    public int getIdProfil() {
+        return idProfil;
+    }
+
+    public void setIdProfil(int idProfil) {
+        this.idProfil = idProfil;
+    }
+
+    public String getNomProfil() {
+        return nomProfil;
+    }
+
+    public void setNomProfil(String nomProfil) {
+        this.nomProfil = nomProfil;
+    }
+
+    public Collaborateur getCollaborateur() {
+        return collaborateur;
+    }
+
+    public void setCollaborateur(Collaborateur collaborateur) {
+        this.collaborateur = collaborateur;
+    }
+}
